7f64018e81db8f4ad60c177aba0f69b4bcccccfb
maga4208 <spotid152@gmail.com>
2026-07-16 09:05:09 +0800

    Update SceneDelegate.swift

--- SimpleIOSApp/SceneDelegate.swift
+++ SimpleIOSApp/SceneDelegate.swift
@@ -7,6 +7,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
+        // Создаём главный экран (это будет наш экран входа)
         let viewController = ViewController()
         window?.rootViewController = UINavigationController(rootViewController: viewController)
         window?.makeKeyAndVisible()
